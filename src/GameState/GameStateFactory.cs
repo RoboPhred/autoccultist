3a29bbddb28efd9733fc169b26f4c60372280df2
RoboPhred <robophred@gmail.com>
2020-08-13 15:28:01 -0700

    Note on consumed cards

--- src/GameState/GameStateFactory.cs
+++ src/GameState/GameStateFactory.cs
@@ -4,7 +4,8 @@ namespace Autoccultist.GameState
     {
         public static IGameState FromCurentState()
         {
-
+            // Game state should be derived from the game every frame,
+            //  but consumed or reserved cards need to persist between frames
         }
     }
 }
