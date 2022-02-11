e7e6398ba5c12beb323e5ca28918a17d4f6f18dc
RoboPhred <robophred@gmail.com>
2022-02-11 10:23:30 -0700

    Conditional card choices

--- src/Config/CardChoices/SlottableCardChooserConfig.cs
+++ src/Config/CardChoices/SlottableCardChooserConfig.cs
@@ -1,11 +1,11 @@
-namespace Autoccultist.Config
+namespace Autoccultist.Config.CardChoices
 {
     using Autoccultist.GameState;
 
     /// <summary>
     /// A config file for specifying cards that are slottable.
     /// </summary>
-    public class SlottableCardChoiceConfig : CardChooserConfig, ISlottableCardChoiceConfig
+    public class SlottableCardChooserConfig : CardChooserConfig, ISlottableCardChoiceConfig
     {
         /// <inheritdoc/>
         protected override bool AdditionalFilter(ICardState card)
