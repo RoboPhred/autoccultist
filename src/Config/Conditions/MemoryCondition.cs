e5c6e857f3b6a12e9039bd66b16400177b40a270
RoboPhred <robophred@gmail.com>
2023-06-26 15:24:50 -0700

    Suppress errors for initial checkup of conditions

--- src/Config/Conditions/MemoryCondition.cs
+++ src/Config/Conditions/MemoryCondition.cs
@@ -9,6 +9,19 @@ namespace AutoccultistNS.Config.Conditions
 
         public override ConditionResult IsConditionMet(IGameState state)
         {
+            // This is called when the game is not running???
+            if (!GameAPI.IsRunning)
+            {
+                try
+                {
+                    throw new System.Exception("debug");
+                }
+                catch (System.Exception ex)
+                {
+                    NoonUtility.LogWarning($"Ignoring attempt to check memory condition while game is not running: {ex.ToString()}");
+                    return ConditionResult.Failure;
+                }
+            }
             // TODO: Bit awkward calling out to Hippocampus directly.  Should probably store memory values in IGameState
             // You know, for that unit testing i'm never going to do.
             if (!this.Value.IsConditionMet(Hippocampus.GetMemory(this.Memory)))
